db8e3af884ce1d1394fd4c9ea54b077b5ddb1c1f
Enes Bal <ce.enesbal@gmail.com>
2017-10-28 14:29:00 +0300

    no message

---
+++ MVCpattern/Student.java
@@ -0,0 +1,25 @@
+package MVCpattern;
+
+/**
+ * Created by enes on 10/28/2017.
+ */
+public class Student {
+    private String rollNo;
+    private String name;
+
+    public String getRollNo(){
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo){
+        this.rollNo = rollNo;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
---
+++ MVCpattern/StudentView.java
@@ -0,0 +1,13 @@
+package MVCpattern;
+
+/**
+ * Created by enes on 10/28/2017.
+ */
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+
+}
